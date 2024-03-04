e529cdc86228c7e000a07231699f1dd24d85cbb3
Kartik <kkartik@nvidia.com>
2024-03-04 16:11:25 +0530

    soc/tegra: fuse: Remove security_mode fuse from keepout list
    nv_fuse_read.sh script allows reading security_mode fuse from userspace.
    
    Update tegra234 nvmem keepout list by removing security_mode fuse from
    it.
    
    Bug 4408388
    
    Change-Id: I31ca95a9e044a9cb43893f2000d7cc5c391415f6
    Signed-off-by: Kartik <kkartik@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-jammy/+/3088689
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Ahung Cheng <ahcheng@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/soc/tegra/fuse/fuse-tegra30.c
+++ drivers/soc/tegra/fuse/fuse-tegra30.c
@@ -647,8 +647,11 @@ static const struct nvmem_cell_lookup tegra234_fuse_lookups[] = {
 };
 
 static const struct nvmem_keepout tegra234_fuse_keepouts[] = {
-	{ .start = 0x01c, .end = 0x0c8 },
-	{ .start = 0x12c, .end = 0x184 },
+	{ .start = 0x01c, .end = 0x064 },
+	{ .start = 0x084, .end = 0x0a0 },
+	{ .start = 0x0a4, .end = 0x0c8 },
+	{ .start = 0x12c, .end = 0x164 },
+	{ .start = 0x16c, .end = 0x184 },
 	{ .start = 0x190, .end = 0x198 },
 	{ .start = 0x1a0, .end = 0x204 },
 	{ .start = 0x21c, .end = 0x250 },
