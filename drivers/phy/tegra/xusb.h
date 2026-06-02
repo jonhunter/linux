8c3ef65ecf0b0c32b41c1b4d4c4dd990fcd4aa07
Jon Hunter <jonathanh@nvidia.com>
2026-06-02 11:05:19 +0000

    phy: tegra: Add support for Tegra238 XUSB pad controller
    Add support for the Tegra238 XUSB pad controller by populating the
    Tegra238 specific SoC data for the controller. The Tegra238 has three
    USB2 and USB3 ports, uses the same port ops as Tegra186 and has the
    same supplies as Tegra194. Due to the dependencies the Tegra238 device
    has on the Tegra186 port ops and Tegra194 supplies, drop the compile
    time guards around these structures.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>

--- drivers/phy/tegra/xusb.h
+++ drivers/phy/tegra/xusb.h
@@ -514,6 +514,9 @@ extern const struct tegra_xusb_padctl_soc tegra194_xusb_padctl_soc;
 #if defined(CONFIG_ARCH_TEGRA_234_SOC)
 extern const struct tegra_xusb_padctl_soc tegra234_xusb_padctl_soc;
 #endif
+#if defined(CONFIG_ARCH_TEGRA_238_SOC)
+extern const struct tegra_xusb_padctl_soc tegra238_xusb_padctl_soc;
+#endif
 #if defined(CONFIG_ARCH_TEGRA_264_SOC)
 extern const struct tegra_xusb_padctl_soc tegra264_xusb_padctl_soc;
 #endif
